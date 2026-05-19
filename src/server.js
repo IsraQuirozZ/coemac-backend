fb3a38eb2c8e331b566429b02dc56dbeced7cfda
sebascordovaaa <sebascordovalaguna@icloud.com>
2026-05-19 10:23:18 +0200

    pruebas smtpass

--- src/server.js
+++ src/server.js
@@ -5,4 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
+  console.log("PASS:", process.env.SMTP_PASS);
+
 });
