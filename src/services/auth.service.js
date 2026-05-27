1b1d6a603d67ba09e66e36e68d059cde7672be51
sebascordovaaa <sebascordovalaguna@icloud.com>
2026-05-27 11:57:37 +0200

    prueba de correo

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -20,6 +20,9 @@ const getTransporter = () => {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,  
+      },
     });
   }
   return transporter;
