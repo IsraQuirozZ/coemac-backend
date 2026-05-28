2732d54fb53240afbdb838a4c2bce3530122980d
sebascordovaaa <sebascordovalaguna@icloud.com>
2026-05-28 19:57:09 +0200

    verify

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -15,7 +15,7 @@ const register = async (req, res, next) => {
 
 const verifyEmail = async (req, res, next) => {
   try {
-    const result = await authService.verifyEmail(req.query);
+    const result = await authService.verifyEmail(req.body);
 
     res.status(200).json({
       success: true,
